3dcb4f3984e1372db5acf6f60f81292c1dba588c
blylei <blylei.info@gmail.com>
2023-03-17 00:33:47 +0800

    init

--- server/service/backup.go
+++ server/service/backup.go
@@ -8,6 +8,56 @@ This file is part of Frabit
 
 package service
 
+import (
+	"context"
+	"time"
+
+	"github.com/frabits/frabit/common/log"
+	pxb "github.com/frabits/frabit/pkg/xtrabackup"
+
+	"go.uber.org/zap"
+)
+
+type BackupType string
+
+const (
+	Logical  BackupType = "logical"
+	Physical BackupType = "physical"
+)
+
+// BackupService implement DB backup task
 type BackupService struct {
-	Id int
+	StartDatetime time.Time
+	EndDatetime   time.Time
+	PXB           pxb.Xtrabackup
+	Type          BackupType
+	Logger        *zap.Logger
+}
+
+func newBackupService() *BackupService {
+	log.Info("create BackupService")
+	return &BackupService{
+		Logger: log.Logger,
+	}
+}
+
+// Start a backup job
+func (bak *BackupService) Start(ctx context.Context, bakType BackupType) error {
+	bak.Logger.Info("create BackupService")
+	bak.Type = bakType
+	return nil
+}
+
+func (bak *BackupService) Stop() error {
+
+	return nil
+}
+
+func (bak *BackupService) Cancel() error {
+
+	return nil
+}
+
+func init() {
+	newBackupService()
 }
