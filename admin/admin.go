6eb7f45096eb862acd426ff6f3339a644777af7c
blylei <blylei.info@gmail.com>
2023-03-25 00:50:03 +0800

    init

--- admin/admin.go
+++ admin/admin.go
@@ -14,27 +14,3 @@
 // limitations under the License.
 
 package main
-
-import (
-	"fmt"
-	"time"
-)
-
-func main() {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-	done := make(chan bool)
-	go func() {
-		time.Sleep(10 * time.Second)
-		done <- true
-	}()
-	for {
-		select {
-		case <-done:
-			fmt.Println("Done!")
-			return
-		case t := <-ticker.C:
-			fmt.Println("Current time: ", t.Format(time.RFC3339))
-		}
-	}
-}
